8b9967c772580b2a8e3d662c81ee322af7808be1
joaoncardoso <joao.nuno380@gmail.com>
2021-12-28 20:30:12 +0000

    Problem 2, doesn't work

--- proj.cpp
+++ proj.cpp
@@ -24,7 +24,6 @@ int main() {
 }
 
 string problem1() {
-
     vector<int> seq = readToVector();
     vector<int> lcs (1, 1);
     vector<int> freq (1, 1);
@@ -55,10 +54,35 @@ string problem1() {
 }
 
 string problem2() {
+    // não funciona - ao preencher a tabela para 1 2 6 3 7 e 1 2 4 7 a maior subsequência comum até ao 3 do primeiro vetor é 3, mas até ao 7 não é 3+1 porque esse 3 englobava a subsequência 1 2 3 no segundo que não poderia ter o 7 a seguir
 
     vector<int> seq1 = readToVector();
     vector<int> seq2 = readToVector();
-    return "TODO: Problem 2";
+    int maxn = 0, pos_seq2, size1 = seq1.size(), size2 = seq2.size();
+    vector<int> lcs (size1, 0);
+    bool unmatched;
+    for (int i = 0; i < size1; i++) {
+        unmatched = true;
+        for (int j = size2 - 1; j >= 0; j--) {
+            if (seq1[i] == seq2[j]){
+                unmatched = false;
+                pos_seq2 = j;
+                break;
+            }
+        }
+        if (unmatched) continue;
+        for (int k = 0; k <= pos_seq2; k++) {
+            if (seq1[k] < seq1[i]) {
+                lcs[i] = max(lcs[k] + 1, lcs[i]);
+            }
+        }
+        lcs[i] = max(lcs[i], 1);
+    }
+    for (int i = 0; i < size1; i++) {
+        cout << lcs[i];
+        maxn = max(lcs[i], maxn);
+    }
+    return '\n' + to_string(maxn) + '\n';
 }
 
 vector<int> readToVector() {
